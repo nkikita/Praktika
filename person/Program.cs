9899836e15c809d05c95c0ff209834923e1e5357
Nikita <nikita.korolev.14.08.2005@mail.ru>
2025-01-21 20:01:27 +0500

    add person

---
+++ person/Program.cs
@@ -0,0 +1,21 @@
+﻿﻿class Program{
+    static void Main(string[] args){
+        Person Nikita = new Person("Никита","Королев",new DateTime(2005, 08, 14,18,33,21), 190);
+        Nikita.write();
+    }
+}
+class Person{
+    string Name {get;}
+    string SurN {get;}
+    DateTime Date_birth{get;}
+    int Height{get;}
+    public Person(string name, string surn, DateTime date_birth, int height){
+        Name = name;
+        SurN = surn;
+        Date_birth = date_birth;
+        Height = height;
+    }
+    public void write(){
+        Console.WriteLine($"Имя: {Name},  Фамилия: {SurN}, Дата рождения: {Date_birth}, Рост: {Height}");
+    }
+}

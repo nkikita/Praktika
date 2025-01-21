87afc838dece4d5e3203335005a597785c15f430
Nikita <nikita.korolev.14.08.2005@mail.ru>
2025-01-21 19:13:09 +0500

    add sringbuilder

---
+++ stringbuilder/Program.cs
@@ -0,0 +1,13 @@
+﻿﻿using System.Text;
+string str = "123456789";
+StringBuilder sb = new StringBuilder();
+foreach(char nu in str){
+    int num = (int)char.GetNumericValue(nu);
+    if (num % 2 == 0) 
+        {
+            sb.Append(num + " ");
+        }
+}
+Console.Write("Четные числа: " + sb);
+
+
